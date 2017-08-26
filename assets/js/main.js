c48129832d02c2d5a16a9a221a73fee292165c9c
Mustafa <mustafa.tasdemir@hotmail.com.tr>
2017-08-26 15:20:39 -0700

    Added some experiences and projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,37 @@
 jQuery(document).ready(function($) {
 
     $('.level-bar-inner').css('width', '0');
-    
+
     $(window).on('load', function() {
 
         $('.level-bar-inner').each(function() {
-        
+
             var itemWidth = $(this).data('level');
-            
+
             $(this).animate({
                 width: itemWidth
             }, 800);
-            
+
         });
 
     });
-   
-    
+
+
+
+    $(".to-project").on('click', function(event) {
+
+     if (this.hash !== "") {
+       event.preventDefault();
+
+       var hash = this.hash;
+
+       $('html, body').animate({
+         scrollTop: $(hash).offset().top
+       }, 300, function(){
+         window.location.hash = hash;
+       });
+     } // End if
+   });
+
 
 });
